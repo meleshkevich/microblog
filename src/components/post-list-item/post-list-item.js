e1d190e44f312c0f915272cbe9968ee08d0df203
Ilja home <i.meleshkevich@gmail.com>
2020-07-18 00:27:11 +0200

    adding onImportant and onLike func

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -1,19 +1,55 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const PostListItem = ({label, important = false}) => {                
-    let classNames = 'app-list-item d-flex justify-content-between'
+export default class PostListItem extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            important: false,
+            like: false
+        };
+        this.onImportant = this.onImportant.bind(this);
+        this.onLike = this.onLike.bind(this);  // refactor without bind ?
+    }
+
+    onImportant() {
+        this.setState(({important}) => ({
+                important: !important
+        }))
+    }
+
+    onLike() {
+        this.setState(({like}) => ({
+                like: !like
+        }))
+    }
+
+    render() {
+        
+        const { label } = this.props;
+        const { important, like } = this.state;
+
+        let classNames = 'app-list-item d-flex justify-content-between';
+        
         if(important) {
             classNames += ' important';
         }
-    return (
+        
+        if(like) {
+            classNames += ' like';
+        }
+
+        return (
         <div className= {classNames}> 
-            <span className='app-list-item-label'>
+            <span className='app-list-item-label'
+            onClick = {this.onLike}>
                 {label}                                           
             </span>
             <div className='d-flex justify-content-center align-items-center'> 
                 <button 
                 type='button' 
-                className='btn-star btn-sm'>
+                className='btn-star btn-sm'
+                onClick = {this.onImportant}>
                     <i className='fa fa-star'></i>
                 </button>
             
@@ -25,7 +61,8 @@ const PostListItem = ({label, important = false}) => {
                      <i className='fa fa-heart'></i>
             </div>
         </div>
-    )
+        )
+    }
 }
 
-export default PostListItem; 
+ 
--- src/index.js
+++ src/index.js
@@ -1,66 +1,9 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app';
 
-// class WhoAmI extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       years: 35
-//     }
-//     this.nextYear = this.nextYear.bind(this);
-//       //   // this.nextYear = () => {
-//       //   //   this.setState(state => ({
-//       //   //     years: ++this.state.years
-//       //   //   }))
-//       //   // }
-    
-//   }
-//   //   state = {
-//   //     years: 35
-//   //   }
-//   // nextYear = () {
-//   //   // console.log(1);
-//   //   this.setState(state => ({
-//   //     years: ++state.years
-//   //   }))
-//   // }
-//   render () {
-//     const {name, surname, link} = this.props;
-//     const {years} = this.state;
-//     return(
-//       <>
-//       <button onClick={this.nextYear}>++</button>
-//       <h1>My name is {name}, surname - {surname}, years = {years}  </h1>
-//       <a href={link}>My profile</a>
-  
-//       </>
-       
-//     )
-//   }
-
-
-// // function WhoAmI({name, surname, link}) {
-// //   return(
-// //     <>
-// //     <h1>My name is {name}, surname - {surname}  </h1>
-// //     <a href={link}>My profile</a>
-
-// //     </>
-     
-// //   )
-// // }
-// }
-// const All = () => {
-//   return (
-//     <>
-//       <WhoAmI name='Mark' surname='Zukerberg' link='tut.by'/>
-//       <WhoAmI name='Ivan' surname='Petrov' link='tut.by'/>
-//       <WhoAmI name='Joko' surname='Ono' link='tut.by'/>
-//     </>
-//   )
-// }
+ 
 ReactDOM.render(
   <React.StrictMode>
     <App/>
